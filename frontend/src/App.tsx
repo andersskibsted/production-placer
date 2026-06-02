adebc65849e0088137601b6c416e69f383fe83e5
Kristian Skovgaard <cbd795@alumni.ku.dk>
2026-06-02 15:16:39 +0200

    Flere frontend ændringer + startscript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,30 +14,37 @@ function App() {
   return (
 <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'start', minHeight: '100vh', gap: '40px' }}>
   {/* <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', gap: '40px' }}> */}
+    <div style={{ position: "relative", width: "190px", height: "180px" }}>
+      <div style={{
+        position: "absolute",
+        width: "200px",
+        height: "200px",
+        borderRadius: "50%",
+        backgroundColor: "#e00000"
+      }}/>
+      <img src="/data/tractor.svg" width={150} height={150} style={{ position: "relative", marginTop: "20px" }}/>
+    </div>
     <h1>Welcome to ProductionPlacer! </h1>
     <p>We are here to help you find the perfect spot for your next business endevour, 
       as long as what you need is raw produce from the wonderful farmers of Denmark!</p>
       {/* <BackgroundMusic /> */}
-      <div>
-        <CropSelector />
-        <RegionSelector />
-      </div>
+
     <h2>You can browse the produce yield for each region by filling in the blanks below <br /> and hit the button "Fetch Data"</h2>
-    <div>
+    <div className="section">
       <ProduceSelector />
     </div>
-    <div>
+    <div className="section">
       <ProduceTable />
     </div>
 
     <div className="section">
       <FindProductionPlace />
     </div>
-    <div>
+    <div className="section">
       <DenmarkMap data={[
       ]} />
     </div>
-    <div>
+    <div className="section">
       <ProductForm />
     </div>
   </div>
--- frontend/src/api/types.ts
+++ frontend/src/api/types.ts
@@ -32,7 +32,7 @@ export interface ProduceRegion {
 }
 
 export interface RegionsWithAvailableProduce {
-  regions: strin[];
+  regions: string[];
 }
 export interface ApiError {
   error: string;
